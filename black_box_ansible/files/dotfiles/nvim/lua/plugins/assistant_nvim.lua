7e576f172101c226b3ec37c6d912fcc7df2f7c4e
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-09-17 23:27:55 +0530

    Remove yazi configuration file (yazi.toml) and associated settings

--- black_box_ansible/files/dotfiles/nvim/lua/plugins/assistant_nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "A7lavinraj/assistant.nvim",
-    event = "VimEnter",
-    dependencies = { "stevearc/dressing.nvim" }, -- optional but recommended
-    keys = {
-        { "<leader>a", "<cmd>AssistantToggle<cr>", desc = "Toggle Assistant.nvim window" },
-    },
-    opts = {},
-}
