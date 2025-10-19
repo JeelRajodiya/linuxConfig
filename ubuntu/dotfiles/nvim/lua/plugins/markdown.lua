78d219073d7bb33b7fe34a3dafd4d76c0c3fb133
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-10-19 19:48:46 +0530

    Add bullets.vim plugin for markdown auto-formatting and configuration

--- ubuntu/dotfiles/nvim/lua/plugins/markdown.lua
+++ ubuntu/dotfiles/nvim/lua/plugins/markdown.lua
@@ -16,4 +16,16 @@ return {
       },
     },
   },
+  -- Auto-formatting for markdown
+  {
+    "bullets-vim/bullets.vim",
+    ft = { "markdown", "text", "gitcommit" },
+    config = function()
+      vim.g.bullets_enabled_file_types = { "markdown", "text", "gitcommit" }
+      vim.g.bullets_enable_in_empty_buffers = 0
+      vim.g.bullets_set_mappings = 1
+      vim.g.bullets_mapping_leader = ""
+      vim.g.bullets_delete_last_bullet_if_empty = 1
+    end,
+  },
 }
