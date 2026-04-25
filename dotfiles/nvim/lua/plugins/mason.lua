153ac9b079d876ca664ec831d6f26fd5d083b248
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2026-04-25 23:47:48 +0530

    fix!

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -62,5 +62,26 @@ return {
                 "biome",
             },
         },
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local mr = require("mason-registry")
+            mr:on("package:install:success", function()
+                vim.defer_fn(function()
+                    require("lazy.core.handler.event").trigger({
+                        event = "FileType",
+                        buf = vim.api.nvim_get_current_buf(),
+                    })
+                end, 100)
+            end)
+
+            mr.refresh(function()
+                for _, tool in ipairs(opts.ensure_installed) do
+                    local ok, p = pcall(mr.get_package, tool)
+                    if ok and not p:is_installed() and not p:is_installing() then
+                        p:install()
+                    end
+                end
+            end)
+        end,
     },
 }
