0af27b05050e9cf1eb083e70e9774702f17637e8
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2026-09-05 23:54:41 +0530

    chore(yazi): remove unused plugins

--- dotfiles/common/.config/yazi/init.lua
+++ dotfiles/common/.config/yazi/init.lua
@@ -48,27 +48,3 @@ require("yamb"):setup({
 })
 
 require("git"):setup()
-
--- Disabled because this revision breaks rendering on Yazi 26.5.6.
---[[ require("yaziline"):setup({
-    -- color = "#98c379", -- main theme color
-    default_files_color = "darkgray", -- color of the file counter when it's inactive
-    selected_files_color = "white",
-    yanked_files_color = "green",
-    cut_files_color = "red",
-
-    separator_style = "curvy", -- "angly" | "curvy" | "liney" | "empty"
-    separator_open = "",
-    separator_close = "",
-    separator_open_thin = "",
-    separator_close_thin = "",
-    separator_head = "",
-    separator_tail = "",
-
-    select_symbol = "",
-    yank_symbol = "󰆐",
-
-    filename_max_length = 24,         -- truncate when filename > 24
-    filename_truncate_length = 6,     -- leave 6 chars on both sides
-    filename_truncate_separator = "...", -- the separator of the truncated filename
-}) ]]
